99b4be7a3ee03d00d3bcd029ec014f3c20bc2c7c
jak010 <bluetoon@naver.com>
2024-08-10 18:27:00 +0900

    add, java practice

---
+++ agilecodes/java_examples/src/SetExample.java
@@ -0,0 +1,8 @@
+public class SetExample {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ practices/programmers/src/programmers120818.java
@@ -0,0 +1,31 @@
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120818
+ */
+public class programmers120818 {
+
+
+    public static int solution(int price) {
+        int answer = 0;
+
+        if (price >= 100_000 && price < 300_000) {
+            answer = (int) (price - (price * 0.05));
+        } else if (price >= 300_000 && price < 500_000) {
+            answer = (int) (price - (price * 0.1));
+        } else if (price >= 500_000) {
+            answer = (int) (price - (price * 0.2));
+        } else {
+            return price;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(150_000));
+        System.out.println(solution(580_000));
+
+
+    }
+
+}
