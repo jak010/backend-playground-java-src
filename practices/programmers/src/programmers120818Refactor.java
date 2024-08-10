082d58dba01461592a6f5c2efd8f044d76853231
jak010 <bluetoon@naver.com>
2024-08-10 19:50:06 +0900

    add2, java practice with Programmers120818

--- practices/programmers/src/programmers120818Refactor.java
+++ practices/programmers/src/programmers120818Refactor.java
@@ -1,80 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedMap;
 
-/** https://school.programmers.co.kr/learn/courses/30/lessons/120818
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120818
  * 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
  * 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
  * <p>
  * 할인 정책 - DiscountPolicy
- * 할인율   - DiscountThreshold
+ * 할인율    - DiscountThreshold
+ * 가격     - amount
  */
 
 
 interface DiscountPolicy {
 
 
-    int applyDiscount(int price);
+    int applyPolicy(int price);
 
+    boolean isApplyDiscountRate(int price);
 
 }
 
-abstract class AbstractDiscount {
+abstract class AbstractDiscountThresHold {
     Double discountRate = 0.0;
-    int max_price = 0;
-    int min_price = 0;
+    int maxThreadHold = 0;
+    int minThreadHold = 0;
 
 
-    public boolean isApplyDiscountRate(int price) {
-        return (price >= this.min_price) && (price < this.max_price);
-    }
 }
 
 
-class BronzeDiscountPolicy extends AbstractDiscount implements DiscountPolicy {
+class BronzeDiscountPolicy extends AbstractDiscountThresHold implements DiscountPolicy {
 
     public BronzeDiscountPolicy() {
         this.discountRate = 0.95;
-        this.max_price = 300_000;
-        this.min_price = 100_000;
+        this.maxThreadHold = 300_000;
+        this.minThreadHold = 100_000;
     }
 
     @Override
-    public int applyDiscount(int price) {
-        if (isApplyDiscountRate(price)) {
-            return (int) (price * this.discountRate);
-        }
-        return price;
-    }
-}
-
-
-class SilverDiscountPolicy extends AbstractDiscount implements DiscountPolicy {
-
-    public SilverDiscountPolicy() {
-        this.discountRate = 0.9;
-        this.max_price = 500_000;
-        this.min_price = 300_000;
-    }
-
-    @Override
-    public int applyDiscount(int price) {
-        if (isApplyDiscountRate(price)) {
-            return (int) (price * this.discountRate);
-        }
-        return price;
-    }
-}
-
-class GoleDiscountPolicy extends AbstractDiscount implements DiscountPolicy {
-
-    public GoleDiscountPolicy() {
-        this.discountRate = 0.8;
-        this.min_price = 500_000;
-    }
-
-    @Override
-    public int applyDiscount(int price) {
+    public int applyPolicy(int price) {
         if (isApplyDiscountRate(price)) {
             return (int) (price * this.discountRate);
         }
@@ -83,7 +48,52 @@ class GoleDiscountPolicy extends AbstractDiscount implements DiscountPolicy {
 
     @Override
     public boolean isApplyDiscountRate(int price) {
-        return price >= this.min_price;
+        return (price >= this.minThreadHold) && (price < this.maxThreadHold);
+    }
+}
+
+
+class SilverDiscountPolicy extends AbstractDiscountThresHold implements DiscountPolicy {
+
+    public SilverDiscountPolicy() {
+        this.discountRate = 0.9;
+        this.maxThreadHold = 500_000;
+        this.minThreadHold = 300_000;
+    }
+
+    @Override
+    public int applyPolicy(int price) {
+        if (isApplyDiscountRate(price)) {
+            return (int) (price * this.discountRate);
+        }
+        return price;
+    }
+
+    @Override
+    public boolean isApplyDiscountRate(int price) {
+        return (price >= this.minThreadHold) && (price < this.maxThreadHold);
+    }
+
+}
+
+class GoldDiscountPolicy extends AbstractDiscountThresHold implements DiscountPolicy {
+
+    public GoldDiscountPolicy() {
+        this.discountRate = 0.8;
+        this.minThreadHold = 500_000;
+    }
+
+    @Override
+    public int applyPolicy(int price) {
+        if (isApplyDiscountRate(price)) {
+            return (int) (price * this.discountRate);
+        }
+        return price;
+    }
+
+    @Override
+    public boolean isApplyDiscountRate(int price) {
+        return price >= this.minThreadHold;
     }
 }
 
@@ -93,15 +103,14 @@ public class programmers120818Refactor {
 
     public static int solution(int price) {
         int discountAmount = 0;
-        int answer = 0;
 
         List<DiscountPolicy> policies = new ArrayList<>();
-        policies.add(new GoleDiscountPolicy());
+        policies.add(new GoldDiscountPolicy());
         policies.add(new SilverDiscountPolicy());
         policies.add(new BronzeDiscountPolicy());
 
         for (DiscountPolicy policy : policies) {
-            discountAmount = policy.applyDiscount(price);
+            discountAmount = policy.applyPolicy(price);
             if (price != discountAmount) {
                 return discountAmount;
             }
@@ -113,6 +122,4 @@ public class programmers120818Refactor {
         System.out.println(solution(150_000));
         System.out.println(solution(580_000));
     }
-
-
 }
