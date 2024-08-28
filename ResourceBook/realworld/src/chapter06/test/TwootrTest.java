01c083701aa4d0d06f32a376ab4c7dc0a18a9c61
jak010 <bluetoon@naver.com>
2024-08-29 01:26:50 +0900

    [add]real-world software development : chapter06 - 151page, TwooterTest Class

---
+++ ResourceBook/realworld/src/chapter06/test/TwootrTest.java
@@ -0,0 +1,20 @@
+package chapter06.test;
+
+import org.junit.jupiter.api.Test;
+
+public class TwootrTest {
+
+    //  151page
+    @Test
+    public void shouldBeAbleTOAuthenticationUser() {
+
+        // 유효 사용자의 로그온 메시지 수신
+
+        // 로그온 메서드는 새 엔드포인트 반환
+
+        // 엔드포인트 유효성을 확인하는 어서션
+
+
+    }
+
+}
